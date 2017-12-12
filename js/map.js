0571b3e079b5a22f282eb4c2da4211e89baa6368
anna-voil <a.voil.63@gmail.com>
2017-12-12 23:11:38 +0400

    Задание 14 Переписана функция синхронизации полей

--- js/map.js
+++ js/map.js
@@ -172,24 +172,41 @@ userMapPin.addEventListener('keydown', function (event) {
 });
 userMapPin.addEventListener('mouseup', showMain);
 
-document.querySelector('[name=timein]').addEventListener('change', function (event) {
-  var optionIndex = event.target.selectedIndex;
-  document.querySelector('[name=timeout]').selectedIndex = optionIndex;
-});
-
-var TYPES_PRICE_DICTIONARY = {'flat': '1000', 'house': '5000', 'bungalo': '0', 'palace': '10000'};
-
-
-var typeElement = document.querySelector('[name=type]');
-typeElement.addEventListener('change', function () {
-  var price = TYPES_PRICE_DICTIONARY[typeElement.value];
-  document.querySelector('[name=price]').value = price;
-});
-
-var ROOMS_DICTIONARY = {'1': '1', '2': '2', '3': '3', '100': '0'};
 
 var ROOMS_ELEMENT = document.querySelector('[name=rooms]');
 var GUESTS_ELEMENT = document.querySelector('[name=capacity]');
-ROOMS_ELEMENT.addEventListener('change', function () {
-  GUESTS_ELEMENT.value = ROOMS_DICTIONARY[ROOMS_ELEMENT.value];
-});
+var ROOMS_ORDER = ['1', '2', '3', '100'];
+var GUESTS_ORDER = ['1', '2', '3', '0'];
+
+var CHECK_IN_ELEMENT = document.querySelector('[name=timein]');
+var CHECK_OUT_ELEMENT = document.querySelector('[name=timeout]');
+
+var TYPE_ELEMENT = document.querySelector('[name=type]');
+var TYPE_ORDER = HOUSE_TYPE;
+var PRICE_ELEMENT = document.querySelector('[name=price]');
+var MIN_PRICE_ORDER = ['1000', '5000', '0', '10000'];
+
+
+function setValue(element, value) { // устанавливает значение value элементу input
+  element.value = value;
+}
+function setMin(element, value) {
+  element.min = value;
+}
+
+function synchronizeInputs(element1, array1, element2, array2, callback) { // синхронизирует значения 2-х input-ов
+  var cb = callback || setValue;
+  function onChange() {
+    var targetElementValue = array2[array1.indexOf(element1.value)];
+    cb(element2, targetElementValue);
+  }
+  element1.addEventListener('change', onChange); // синхронизация при изменении значения эл-та
+  onChange(); // синхронизация сразу при загрузке страницы
+}
+synchronizeInputs(ROOMS_ELEMENT, ROOMS_ORDER, GUESTS_ELEMENT, GUESTS_ORDER);
+
+synchronizeInputs(CHECK_IN_ELEMENT, CHECK_IN, CHECK_OUT_ELEMENT, CHECK_OUT);
+synchronizeInputs(CHECK_OUT_ELEMENT, CHECK_OUT, CHECK_IN_ELEMENT, CHECK_IN);
+
+synchronizeInputs(TYPE_ELEMENT, TYPE_ORDER, PRICE_ELEMENT, MIN_PRICE_ORDER, setMin);
+
